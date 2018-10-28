6d7f9098a087eb59c0571008612ede2c98f51149
Christo <christo@cryptodrunks.net>
2018-10-28 11:38:26 +0100

    renamed data process

--- bin/search-media-ddg.sh
+++ bin/search-media-ddg.sh
@@ -2,7 +2,8 @@
 DATE=$(date +%Y-%m-%d)
 IFS=$'\n'
 set -f
-SEARCH_TERMS="./queries/search-terms.txt"
+SEARCH_TERMS="./queries/search-terms-media.txt"
+SEARCH_MODIFIERS="./queries/media-searches-modifier.txt"
 MEDIA_SITES=$1
 counter=0
 
@@ -18,34 +19,39 @@ echo "Starting media website queries"
 
 for i in $(cat < "$SEARCH_TERMS")
 do
-  for j in $(cat < "$MEDIA_SITES")
+  for j in $(cat < "$SEARCH_MODIFIERS")
   do
-    if [[ $((counter % 20)) -eq 0 ]]; then
-      echo "Processed $counter queries"
-    fi
+    for k in $(cat < "$MEDIA_SITES")
+    do
+      if [[ $((counter % 20)) -eq 0 ]]; then
+        echo "Processed $counter queries"
+      fi
 
-    echo "$i,,$j" | jq --slurp \
-                       --raw-input \
-                       --arg q "'" \
-                       'split("\n")[:-1] |
+      echo "$i,,$j,,$k" | jq --slurp \
+                             --raw-input \
+                             --arg q "'" \
+                             'split("\n")[:-1] |
                          map(split(",,") |
                          [{
                            search_batch: ["media website"],
                            search_category: [.[0]]
                           }, {
                            type: "ddg_search",
-                           term: "\($q)\(.[0])\($q) site:\(.[1])"
+                           term: "\($q)\(.[0])\($q) \(.[1]) site:\(.[2])"
                           }]
                          ) |
                          flatten' > q.json
+      echo $(cat q.json)
 
-    doit "q.json" | tee -a ./logs/ddg-media-"$DATE".log
 
-    rm q.json
+      doit "q.json" | tee -a ./logs/ddg-media-"$DATE".log
 
-    WAIT_TIME=$(( ( RANDOM % 300)  + 120 ))
-    echo "Sleeping for $WAIT_TIME seconds."
-    sleep $WAIT_TIME
-    counter=$((counter+1))
+      rm q.json
+
+      WAIT_TIME=$(( ( RANDOM % 300)  + 120 ))
+      echo "Sleeping for $WAIT_TIME seconds."
+      sleep $WAIT_TIME
+      counter=$((counter+1))
+    done
   done
 done
