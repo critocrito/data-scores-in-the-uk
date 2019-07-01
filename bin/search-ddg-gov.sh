95c64b05502f71c53cbc802b3216cbc98170c90a
Christo <christo@cryptodrunks.net>
2019-07-01 11:20:22 +0200

    refactor: avoid quoting problems with JSON on the shell

--- bin/search-ddg-gov.sh
+++ bin/search-ddg-gov.sh
@@ -1,33 +1,43 @@
 #!/bin/sh
 DATE=$(date +%Y-%m-%d)
-IFS=$'\n'
-set -f
 SEARCH_TERMS="./queries/search-terms.txt"
 counter=0
 
 doit() {
   "$(npm bin)"/sugarcube -c pipelines/search-duckduckgo.json \
-              -Q ddg_search:'"'"$1"'" site:gov.uk' \
               -Q tika_location_field:href \
-              -Q workflow_merge:'{"search_batch":["government website"],"search_category":["'"$1"'"]}' \
+              -q "$1" \
               -d
-
 }
-
-export -f doit
 
 echo "Starting government website queries"
 
-for i in $(cat < "$SEARCH_TERMS")
+while IFS='\n' read -r i
 do
-  if [[ $((counter % 20)) -eq 0 ]]; then
+  if [ $((counter % 20)) -eq 0 ]; then
     echo "Processed $counter queries"
   fi
 
-  doit "$i" | tee -a ./logs/ddg-gov-"$DATE".log
+  echo "$i,,gov.uk" | jq --slurp \
+                         --raw-input \
+                         --arg q "'" \
+                         'split("\n")[:-1] |
+                         map(split(",,") |
+                         [{type: "workflow_merge",
+                          term: {
+                            search_batch: ["government"],
+                            search_category: [.[0]]
+                           }}, {
+                           type: "ddg_search",
+                           term: "\($q)\(.[0])\($q) site:\(.[1])"
+                          }]
+                         ) |
+                         flatten' > q.json
+
+  doit "q.json" | tee -a ./logs/ddg-gov-"$DATE".log
 
   WAIT_TIME=$(( ( RANDOM % 300)  + 120 ))
   echo "Sleeping for $WAIT_TIME seconds."
   sleep $WAIT_TIME
   counter=$((counter+1))
-done
+done < "$SEARCH_TERMS"
